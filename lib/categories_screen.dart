9ff1fcb402d1efa4670dfa2b81f52c32e985a226
unknown <kh82matar@gmail.com>
2021-04-26 06:10:09 +0300

    2 commit

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -11,19 +11,20 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('DeliMeal'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
             .map(
               (catData) => CategoryItem(
-                    catData.title,
-                    catData.color,
-                  ),
+                catData.title,
+                catData.color,
+              ),
             )
             .toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
           maxCrossAxisExtent: 200,
           childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
+          crossAxisSpacing: 50,
+          mainAxisSpacing: 50,
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
+      title: 'DeliMeals 1',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'RaleWay',
+          textTheme: ThemeData.light().textTheme.copyWith(
+                subtitle1: TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                subtitle2: TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                headline1: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'RobotoCondensed',
+                    fontWeight: FontWeight.bold),
+              )),
       home: CategoriesScreen(),
     );
   }
